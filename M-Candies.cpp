f4b54a12c1478d1a38427ac6885f5888064c5f62
itzRaghav14 <98763689+itzRaghav14@users.noreply.github.com>
2023-09-03 20:16:56 +0530

    Create M-Candies.cpp

---
+++ M-Candies.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define dbg(...) ; 
+#define debug(...) ; 
+#define crndl ; 
+#endif
+
+const int N = 101;
+const int K = 1e5 + 5;
+const int MOD = 1e9 + 7;
+
+struct Mint {
+    int val;
+ 
+    Mint(long long v = 0) {
+        if (v < 0)
+            v = v % MOD + MOD;
+        if (v >= MOD)
+            v %= MOD;
+        val = v;
+    }
+ 
+    static int mod_inv(int a, int m = MOD) {
+        int g = m, r = a, x = 0, y = 1;
+        while (r != 0) {
+            int q = g / r;
+            g %= r; swap(g, r);
+            x -= q * y; swap(x, y);
+        } 
+        return x < 0 ? x + m : x;
+    } 
+    explicit operator int() const {
+        return val;
+    }
+    Mint& operator+=(const Mint &other) {
+        val += other.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    Mint& operator-=(const Mint &other) {
+        val -= other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+           #if !defined(_WIN32) || defined(_WIN64)
+                return x % m;
+           #endif
+           unsigned x_high = x >> 32, x_low = (unsigned) x;
+           unsigned quot, rem;
+           asm("divl %4\n"
+            : "=a" (quot), "=d" (rem)
+            : "d" (x_high), "a" (x_low), "r" (m));
+           return rem;
+    }
+    Mint& operator*=(const Mint &other) {
+        val = fast_mod((uint64_t) val * other.val);
+        return *this;
+    }
+    Mint& operator/=(const Mint &other) {
+        return *this *= other.inv();
+    }
+    friend Mint operator+(const Mint &a, const Mint &b) { return Mint(a) += b; }
+    friend Mint operator-(const Mint &a, const Mint &b) { return Mint(a) -= b; }
+    friend Mint operator*(const Mint &a, const Mint &b) { return Mint(a) *= b; }
+    friend Mint operator/(const Mint &a, const Mint &b) { return Mint(a) /= b; }
+    Mint& operator++() {
+        val = val == MOD - 1 ? 0 : val + 1;
+        return *this;
+    }
+    Mint& operator--() {
+        val = val == 0 ? MOD - 1 : val - 1;
+        return *this;
+    }
+    // friend Mint operator<=(const Mint &a, const Mint &b) { return (int)a <= (int)b; }
+    Mint operator++(int32_t) { Mint before = *this; ++*this; return before; }
+    Mint operator--(int32_t) { Mint before = *this; --*this; return before; }
+    Mint operator-() const {
+        return val == 0 ? 0 : MOD - val;
+    }
+    bool operator==(const Mint &other) const { return val == other.val; }
+    bool operator!=(const Mint &other) const { return val != other.val; }
+    Mint inv() const {
+        return mod_inv(val);
+    }
+    Mint power(long long p) const {
+        assert(p >= 0);
+        Mint a = *this, result = 1;
+        while (p > 0) {
+            if (p & 1)
+                result *= a;
+ 
+            a *= a;
+            p >>= 1;
+        }
+        return result;
+    }
+    friend ostream& operator << (ostream &stream, const Mint &m) {
+        return stream << m.val;
+    }
+    friend istream& operator >> (istream &stream, Mint &m) {
+        return stream>>m.val;   
+    }
+};
+
+int a[N];
+Mint dp[2][K], pre[2][K];
+
+void solve() {
+	
+	int n, k;
+	cin >> n >> k;
+	
+	for(int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	
+	auto resetDp = [&] () {
+		for(int i = 0; i <= k; i++) {
+			dp[1][i] = 0;
+			pre[1][i] = 0;
+		}
+	};
+	
+	auto getSum = [&] (int l, int r) {
+		if(l <= 0) return pre[0][r];
+		return pre[0][r] - pre[0][l - 1] + MOD;
+	};
+	
+	for(int ind = 0; ind < n; ind++) {
+		resetDp();
+		
+		for(int left = 0; left <= k; left++) {
+			if(ind == 0 && left <= a[ind]) {
+				dp[1][left] = 1;
+				continue;
+			}
+			dp[1][left] += getSum(left - a[ind], left);
+		}
+		
+		pre[1][0] = dp[1][0];
+		for(int left = 1; left <= k; left++) {
+			pre[1][left] = pre[1][left - 1] + dp[1][left];
+		}
+		
+		swap(dp[0], dp[1]);
+		swap(pre[0], pre[1]);
+	}
+	
+	cout << dp[0][k] << endl;
+	
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+    freopen("io/input.txt", "r", stdin);
+    freopen("io/output.txt", "w", stdout);
+    freopen("io/error.txt", "w", stderr);
+    #endif
+	
+	int t = 1;
+	// cin >> t;
+	
+	for(int _t = 1; _t <= t; _t++) {
+		debug(Testcase, _t);
+		solve();
+		crndl;
+	}
+	
+	return 0;
+}
